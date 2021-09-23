37b40e2fd37417accfca87fee2a3c4980901fb48
Jack Hou <jackhou147@berkeley.edu>
2021-09-22 20:14:37 -0700

    added particle class

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 
 console.log("hello world")
+
+class particle {
+    constructor(energy,position=0){
+        this._energy = energy;
+        this._position = position;
+    }
+    get energy(){
+        return this._energy;
+    }
+    set energy(newEnergy){
+        this._energy = newEnergy;
+    }
+    get position(){
+        return this._position;
+    }
+    set position(newPosition){
+        this._position = newPosition;
+    }
+}
+
+
+var A = new particle(10)
+
+console.log(A.energy)
+
