53a8eeb79afb88eb75de6cb94c329fe18ec1bc1d
Jack Hou <jackhou147@berkeley.edu>
2021-10-04 19:14:59 -0700

    rewrote Controller file so that it's now a class

--- Controller.mjs
+++ Controller.mjs
@@ -2,15 +2,28 @@
 import {World, Particle} from "./Model.mjs"
 import {View} from "./View.mjs";
 
-const DELTA_T = 0.1;
-const v = new View(document.getElementById("canvas"));
-var w  = new World();
 
-w.addParticle(new Particle(undefined, [80, 100]));
-w.addParticle(new Particle([0, 100],[0,20]));
+class Controller {
+    #DELTA_T;
+    #view;
+    #world;
+    #id;
+    constructor() {
+        this.#DELTA_T = .1;
+        this.#view = new View(document.getElementById("canvas"));
+        this.#world = new World;
+        this.#world.addParticle(new Particle(undefined, [80, 100]));
+        this.#world.addParticle(new Particle([0, 100],[0,20]));
+    }
 
-let id = setInterval(()=> {
-    w.timeEvolve();
-    v.render(w);
-    console.log("i'm being called.")
-}, DELTA_T);
+    run(){
+        this.#id = setInterval(()=> {
+            this.#world.timeEvolve();
+            this.#view.render(this.#world);
+            console.log("i'm being called.")
+        }, this.#DELTA_T);
+    }
+}
+
+var c = new Controller();
+c.run();
